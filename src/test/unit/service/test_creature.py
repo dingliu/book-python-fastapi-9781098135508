0072ee09332fb5ef5083967dc04a95bfe812f432
Ding Liu <contact@dingliu.me>
2024-08-31 15:40:57 -0400

    chore: tweak unit tests for creature service

--- src/test/unit/service/test_creature.py
+++ src/test/unit/service/test_creature.py
@@ -1,26 +1,33 @@
+import os
+import pytest
 from model.creature import Creature
+from error import MissingError, DuplicateError
+
+os.environ["CRYPTID_SQLITE_DB"] = ":memory:"
 from service import creature as code
 
 
-sample = Creature(
-    name="Yeti",
-    aka="Abominable Snowman",
-    country="CN",
-    area="Himalayas",
-    description="Hirsute Himalayan",
-)
+@pytest.fixture
+def sample() -> Creature:
+    return Creature(
+        name="Yeti",
+        aka="Abominable Snowman",
+        country="CN",
+        area="Himalayas",
+        description="Hirsute Himalayan",
+    )
 
 
-def test_create():
+def test_create(sample):
     resp = code.create(sample)
     assert resp == sample
 
 
-def test_get_exists():
-    resp = code.get_one("Yeti")
+def test_get_exists(sample):
+    resp = code.get_one(sample.name)
     assert resp == sample
 
 
 def test_get_missing():
-    resp = code.get_one("BoxTurtle")
-    assert resp is None
+    with pytest.raises(MissingError):
+        resp = code.get_one("BoxTurtle")
